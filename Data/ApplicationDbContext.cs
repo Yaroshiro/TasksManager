5eb4150434775e728c3414924f5a5568fd72f8d8
Iaroslav <98598112+Yaroshiro@users.noreply.github.com>
2026-07-06 10:10:46 +0900

    Add files via upload

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TasksManager.Models;
+
+namespace TasksManager.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+        public DbSet<ApplicationUser> UsersDB { get; set; }
+        public DbSet<TaskModel> TasksDB { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<ApplicationUser>()
+                    .HasMany(u => u.AssignedTasks)
+                    .WithMany(t => t.AssignedUsers);
+
+            builder.Entity<TaskModel>()
+                .HasOne(t => t.Owner)
+                .WithMany(u => u.OwnedTasks)
+                .HasForeignKey(t => t.OwnerId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
