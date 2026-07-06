b2282693a7c1c801ba74c823b8bf28e28f1eb66e
Iaroslav <98598112+Yaroshiro@users.noreply.github.com>
2026-07-06 09:53:48 +0900

    Add files via upload

---
+++ Areas/Identity/CustomRoles.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+using TasksManager.Models;
+
+namespace TasksManager.Areas.Identity
+{
+    public class CustomRoles
+    {
+        public static async Task AddRoles(IServiceScope scope)
+        {
+            var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            foreach (string role in Enum.GetNames(typeof(ApplicationUser.RoleType)))
+            {
+                if (!await roleManager.RoleExistsAsync(role))
+                {
+                    await roleManager.CreateAsync(new IdentityRole(role));
+                }
+            }
+        }
+    }
+}
